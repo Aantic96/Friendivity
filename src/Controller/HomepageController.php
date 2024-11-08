26388150b03f7fb6eb692c2d38808a9b094f5384
Antonela <gemory.mei@gmail.com>
2024-11-08 14:17:02 +0100

    Finished register and login logic

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,16 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class HomepageController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_homepage')]
-    public function index(): Response
+    #[Route('/', name: 'app_homepage')]
+    public function index(Security $security): Response
     {
-        return $this->render('homepage/index.html.twig', [
+        $values = [
             'controller_name' => 'HomepageController',
-        ]);
+        ];
+
+        $username = $security->getUser()?->getUsername();
+
+        if(!empty($username)) {
+            $values['username'] = $username;
+        }
+
+        return $this->render('homepage/index.html.twig',  $values);
     }
 }
